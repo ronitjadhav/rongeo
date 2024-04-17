49384e623b326fc98d6af79bca7c729f232ba53d
ronitjadhav <ronit.jadhav@camptocamp.com>
2024-04-17 19:15:13 +0200

    Changed the Background and glow

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,12 +6,13 @@ import { HeroHighlight } from "@/app/(home)/components/ui/hero-highlight";
 export default function Page() {
     return (
         <div className='min-h-screen bg-black overflow-hidden'>
-            <HeroHighlight>
+            <div className="dark:bg-black bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2]">
                 <div className="max-w-7xl mx-auto p-5 ">
                     <Navbar/>
                     <HeroSection/>
                 </div>
-            </HeroHighlight>
+            </div>
+
         </div>
     );
 }
