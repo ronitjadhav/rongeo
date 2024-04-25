f8205003f6d348a19d8c6eef902278b132e43abf
ronitjadhav <ronit.jadhav@camptocamp.com>
2024-04-25 13:56:55 +0200

    Added NextUINavbar

---
+++ src/app/(home)/components/NavBarNextUI.tsx
@@ -0,0 +1,53 @@
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@nextui-org/navbar";
+import { SiGithub, SiLinkedin, SiX } from "react-icons/si";
+import React from "react";
+import { AcmeLogo } from "../media/logo.jsx";
+
+export default function NavBarNextUI() {
+    const socials = [
+        {
+            Link: "https://github.com/ronitjadhav",
+            Label: "GitHub",
+            Icon: SiGithub,
+        },
+        {
+            Link: "https://www.linkedin.com/in/ronitjadhav/",
+            Label: "LinkedIn",
+            Icon: SiLinkedin,
+        },
+        {
+            Link: "https://twitter.com/ronit_jadhav",
+            Label: "Twitter",
+            Icon: SiX,
+        },
+    ];
+
+    return (
+        <Navbar shouldHideOnScroll>
+            <NavbarBrand>
+                <AcmeLogo />
+                <p className="font-bold text-inherit">RonGeo 🗺️</p>
+            </NavbarBrand>
+            <NavbarContent justify="end">
+                {socials.map((social, index) => (
+                    <NavbarItem key={index}>
+                        <a
+                            href={social.Link}
+                            aria-label={social.Label}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <social.Icon className="w-5 h-5 hover:scale-125 transition-all"/>
+                        </a>
+                    </NavbarItem>
+                ))}
+                <NavbarItem className="hidden lg:flex">
+                    {/* Add content here */}
+                </NavbarItem>
+                <NavbarItem>
+                    {/* Add content here */}
+                </NavbarItem>
+            </NavbarContent>
+        </Navbar>
+    );
+}
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import Navbar from "@/app/(home)/components/Navbar";
 import HeroSection from "@/app/(home)/components/HeroSection";
 import { HeroHighlight } from "@/app/(home)/components/ui/hero-highlight";
+import NavBarNextUI from "@/app/(home)/components/NavBarNextUI";
 
 export default function Page() {
     return (
         <div className='min-h-screen bg-black overflow-hidden'>
             <div className="dark:bg-black bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] relative">
                 <div className="max-w-7xl mx-auto p-5 ">
-                    <Navbar/>
+                    <NavBarNextUI/>
                     <HeroSection/>
                 </div>
                 <div className="h-10 xl:h-32 bg-gradient-to-t from-black absolute -bottom-5 left-0 xl:bottom-0 w-full">
